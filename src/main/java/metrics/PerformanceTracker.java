0f19373215a03839fbb6430a85460b7032c1c5c4
nauka <241878@astanait.edu.kz>
2025-10-05 19:14:06 +0500

    PerformanceTracker.java

--- src/main/java/metrics/PerformanceTracker.java
+++ src/main/java/metrics/PerformanceTracker.java
@@ -2,28 +2,43 @@ package metrics;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
 
 public class PerformanceTracker {
     private long comparisons = 0;
     private long swaps = 0;
+    private long accesses = 0;
+    private long allocations = 0;
 
     public void addComparison() { comparisons++; }
     public void addSwap() { swaps++; }
+    public void addAccess() { accesses++; }
+    public void addAllocation() { allocations++; }
+
     public long getComparisons() { return comparisons; }
     public long getSwaps() { return swaps; }
+    public long getAccesses() { return accesses; }
+    public long getAllocations() { return allocations; }
 
     public void reset() {
-        comparisons = 0;
-        swaps = 0;
+        comparisons = swaps = accesses = allocations = 0;
     }
 
-    public void exportToCSV(String filename, int size, long timeMs) {
-        boolean fileExists = new java.io.File(filename).exists();
+ 
+    public void exportToPrettyCSV(String filename, int trial, long timeMs, int arraySize) {
+        boolean fileExists = new File(filename).exists();
         try (FileWriter writer = new FileWriter(filename, true)) {
+
             if (!fileExists) {
-                writer.write("ArraySize,Time(ms),Comparisons,Swaps\n");
+                writer.write("| Trial | Time_ms | Comparisons | Swaps  | Accesses | Allocs |\n");
+                writer.write("|-------|----------|-------------|--------|-----------|--------|\n");
             }
-            writer.write(size + "," + timeMs + "," + comparisons + "," + swaps + "\n");
+
+            writer.write(String.format(
+                    "| %-5d | %-8d | %-11d | %-6d | %-9d | %-6d |\n",
+                    trial, timeMs, comparisons, swaps, accesses, allocations
+            ));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
